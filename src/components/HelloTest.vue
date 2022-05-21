0e0cbfeead44331ce20ae1dddedd0fcc2353a7bd
ljh9 <dkssud2208@naver.com>
2022-05-22 06:19:55 +0900

    updated version of vue and edited vue-router

---
+++ src/components/HelloTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+    <h1>ergje3ijro3ijrioejfiwe</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloTest'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <HelloTest /> -->
+    setskemsleg
+    seg
+    seg
+    seg
+    segseg
+    seg
+    seg
+    seg
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloTest from '@/components/HelloTest.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloTest
+//   }
+// }
+</script>
