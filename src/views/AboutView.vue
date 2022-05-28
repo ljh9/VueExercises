53376a4b6803d2575690ecb40e03bff5c6879e41
ljh9 <dkssud2208@naver.com>
2022-05-28 23:56:24 +0900

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h4 class="" :style="st">xxxx테스트</h4>
+      <p>{{price1}} 만원</p>
+    </div>
+    <div>
+      <h4>xxxx테스트</h4>
+      <p>{{price2}} 만원</p>
+    </div>
+    <div class="menu">
+      <a v-for="(a,i) in menu" :key= "i" >{{a}}</a>
+    </div>
   </div>
 </template>
+
+
+<script>
+  export default {
+    name : 'App',
+    data(){
+      return{ 
+        price1 : 60,
+        price2 : 60,
+        st : 'color: blue',
+
+        menu: ['h','g', 'j'],
+      }
+    }
+  }
+</script>
