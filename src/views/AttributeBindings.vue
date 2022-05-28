0e4ccfa9b691e420c45c00ea896ca113e37cef32
ljh9 <dkssud2208@naver.com>
2022-05-29 00:01:28 +0900

    AttributeBindings

---
+++ src/views/AttributeBindings.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            dynamicId: 'testId'
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="exercise3">
+    <!-- <div v-bind:id="dynamicId"></div> -->
+    <div :id="dynamicId"></div>
+  </div>
+</template>
