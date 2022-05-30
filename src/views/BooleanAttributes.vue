08a2f30a4f3f64dd076d39c4c52d6225e70526be
ljh9 <dkssud2208@naver.com>
2022-05-30 18:26:16 +0900

    BooleanAttributes

---
+++ src/views/BooleanAttributes.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            isButtonDisabled: true
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="exercise">
+    <button :disabled="isButtonDisabled">When isButtonDisabled is TRUE</button>
+    <br/>
+    <button :disabled="!isButtonDisabled">When isButtonDisabled is FALSE</button>
+  </div>
+</template>
