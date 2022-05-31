7fa0c591d49336861903ecfe310313e36b5adcbc
ljh9 <dkssud2208@naver.com>
2022-05-31 22:27:45 +0900

    ArgumentsBind

---
+++ src/views/ArgumentsBind.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            url: 'https://www.naver.com'
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="exercise">
+    <a v-bind:href="url">https://www.naver.com</a>
+    <br/>
+    <!-- shorthand -->
+    <a :href="url">https://www.naver.com</a>
+  </div>
+</template>
