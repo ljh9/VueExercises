071220252c161db3f0af1d8d92290317bbf3b06a
ljh9 <dkssud2208@naver.com>
2022-05-31 22:27:00 +0900

    CallingFunctions

---
+++ src/views/CallingFunctions.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    methods: {
+        toGetDate: function () {
+            const today = new Date();
+            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            return date;
+        },
+        toGetTime: function () {
+            const today = new Date();
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            return time;
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="exercise">
+    <span :title="toGetDate()">
+        {{ toGetTime() }}
+    </span>
+  </div>
+</template>
