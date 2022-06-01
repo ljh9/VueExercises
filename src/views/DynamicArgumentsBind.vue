f63cff96d809249e8d935d225457b42bd99860f0
ljh9 <dkssud2208@naver.com>
2022-06-01 22:23:23 +0900

    DynamicArgumentsBind

---
+++ src/views/DynamicArgumentsBind.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            url: 'https://www.naver.com',
+            attributeName: 'href'
+        }
+    }
+}
+</script>
+
+
+<template>
+  <div class="exercise">
+    <a v-bind:[attributeName]="url">https://www.naver.com</a>
+    <br/>
+    <!-- shorthand -->
+    <a :[attributeName]="url">https://www.naver.com</a>
+  </div>
+</template>
