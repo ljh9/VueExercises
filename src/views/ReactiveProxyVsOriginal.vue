2a1f3b1b6f44daf3be2fb05a5ff1b9005856c497
ljh9 <dkssud2208@naver.com>
2022-06-02 23:31:14 +0900

    ReactivityFundamentals

---
+++ src/views/ReactiveProxyVsOriginal.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      someObject: {}
+    }
+  },
+  mounted() {
+    const newObject = {}
+    this.someObject = newObject
+    console.log(newObject === this.someObject) // false
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        Check the console
+    </div>
+</template>
