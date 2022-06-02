7d5ec43df3413c0b12abd99f70e3b82ea9e3b86d
ljh9 <dkssud2208@naver.com>
2022-06-02 23:31:40 +0900

    ReactiveProxyVsOriginal

---
+++ src/views/ReactivityFundamentals.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+  mounted() {
+    console.log(this.count) // => 1
+    this.count = 2
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        Count is: {{ count }}
+    </div>
+</template>
