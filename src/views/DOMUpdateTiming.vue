bb2e77ab0580535af62257dabf6f8cc1338b8b65
ljh9 <dkssud2208@naver.com>
2022-06-03 23:06:11 +0900

    DOMUpdateTiming

---
+++ src/views/DOMUpdateTiming.vue
@@ -0,0 +1,29 @@
+<script>
+import { nextTick } from 'vue'
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+      console.log("before nextTick" + this.count)
+      nextTick(() => {
+        console.log("inside of nextTick" + this.count)
+      })
+      console.log("after nextTick" + this.count)
+    }
+  },
+  mounted() {
+    this.increment()
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="increment">{{ count }}</button>
+    </div>
+</template>
