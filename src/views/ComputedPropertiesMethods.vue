59e58f14f1f695fa6a97bd57b589391458db7be6
ljh9 <dkssud2208@naver.com>
2022-06-04 22:22:16 +0900

    ComputedPropertiesMethods

---
+++ src/views/ComputedPropertiesMethods.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      author: {
+        name: 'J H',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  methods: {
+    calculateBooksMessage() {
+        return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <p>Has published books:</p>
+        <p>{{ calculateBooksMessage() }}</p>
+    </div>
+</template>
