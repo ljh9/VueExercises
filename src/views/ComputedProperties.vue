9226c89ff7b4fdda9d1b78b00437007f3abe649d
ljh9 <dkssud2208@naver.com>
2022-06-04 22:21:58 +0900

    ComputedProperties

---
+++ src/views/ComputedProperties.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      author: {
+        name: 'J H',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    // a computed getter
+    publishedBooksMessage() {
+      // `this` points to the component instance
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <p>Has published books:</p>
+        <span>{{ publishedBooksMessage }}</span>
+    </div>
+</template>
