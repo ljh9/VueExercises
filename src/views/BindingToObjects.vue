a3562b02ba8db184271b09d323de32f017047f17
ljh9 <dkssud2208@naver.com>
2022-06-05 23:43:19 +0900

    BindingToObjects.vue

---
+++ src/views/BindingToObjects.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+        // isActive: true,
+        // hasError: false
+        isActive: false,
+        hasError: true
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div
+            class="static"
+            :class="{ active: isActive, 'text-danger': hasError }"
+        ></div>
+    </div>
+</template>
