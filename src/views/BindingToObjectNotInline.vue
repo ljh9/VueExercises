1ac95c90a9cd74217ad4c59eb6278dac2cc8ad65
ljh9 <dkssud2208@naver.com>
2022-06-05 23:43:36 +0900

    BindingToObjectNotInline

---
+++ src/views/BindingToObjectNotInline.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+        classObject: {
+            active: true,
+            'text-danger': false
+        }
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :class="classObject"></div>
+    </div>
+</template>
