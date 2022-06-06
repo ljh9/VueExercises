24035dab8c29e8ea6542912bdccba78afa0201a5
ljh9 <dkssud2208@naver.com>
2022-06-06 23:15:37 +0900

    BindingToArrays

---
+++ src/views/BindingToArrays.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            activeClass: 'active',
+            errorClass: 'text-danger'
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :class="[activeClass, errorClass]"></div>
+    </div>
+</template>
