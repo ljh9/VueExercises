e7af232dcd32195304ecd8f77be1a5545094859e
ljh9 <dkssud2208@naver.com>
2022-06-06 23:16:02 +0900

    BidingToArraysWithTernaryExpressions

---
+++ src/views/BindingToArraysWithTernaryExpressions.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            activeClass: 'active',
+            errorClass: 'text-danger',
+            isActive: true,
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :class="[isActive ? activeClass : '', errorClass]"></div>
+    </div>
+</template>
