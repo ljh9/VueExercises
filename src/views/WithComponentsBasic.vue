8780a1d73925406f9386bec57bb5860fbe8cd0cc
ljh9 <dkssud2208@naver.com>
2022-06-07 22:14:06 +0900

    WithComponentsBasic

---
+++ src/views/WithComponentsBasic.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyComponent from './MyComponent.vue'
+</script>
+
+<template>
+    <div>
+        <h4>My Component</h4>
+        <!-- when using the component -->
+        <my-component class="baz boo"></my-component>
+    </div>
+</template>
