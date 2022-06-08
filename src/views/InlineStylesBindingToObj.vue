6841cbf5ba11370be782e477ef9063c5ef3bd445
ljh9 <dkssud2208@naver.com>
2022-06-08 22:33:15 +0900

    InlineStylesBindingToObj.vue

---
+++ src/views/InlineStylesBindingToObj.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            activeColor: 'red',
+            fontSize: 30
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :style="{ color: activeColor, fontSize: fontSize + 'px' }">test</div>test
+    </div>
+</template>
