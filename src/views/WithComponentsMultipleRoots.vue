be9423825348ad9a7bffe2eb50866440cf0f7a2d
ljh9 <dkssud2208@naver.com>
2022-06-08 22:32:39 +0900

    WithComponentsMultipleRoots.vue

---
+++ src/views/WithComponentsMultipleRoots.vue
@@ -0,0 +1,15 @@
+<script>
+import MyComponentMultipleRoots from './MyComponentMultipleRoots.vue'
+export default {
+  components: {
+    MyComponentMultipleRoots
+  }
+}
+</script>
+
+<template>
+    <div>
+        <h4>My Component with Multiple Roots</h4>
+        <my-component-multiple-roots class="baz"></my-component-multiple-roots>
+    </div>
+</template>
