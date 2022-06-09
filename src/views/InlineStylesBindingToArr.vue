2fea2083d68b5729f8501ee68763e6b9e7212bf4
ljh9 <dkssud2208@naver.com>
2022-06-09 21:47:14 +0900

    InlineStylesBindingToArr.vue

---
+++ src/views/InlineStylesBindingToArr.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            baseStyles: {
+                color: 'red',
+                fontSize: '13px'
+            },
+            overridingStyles: {
+                fontWeight: 'bold'
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :style="[baseStyles, overridingStyles]">test</div>test
+    </div>
+</template>
