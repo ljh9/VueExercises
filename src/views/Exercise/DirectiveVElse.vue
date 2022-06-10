3e19fbd85620d724b6cd1ec240f6fcd31a22eec7
ljh9 <dkssud2208@naver.com>
2022-06-10 20:31:03 +0900

    DirectiveVElse

---
+++ src/views/Exercise/DirectiveVElse.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+        awesome: true,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="awesome = !awesome">Toggle</button>
+
+        <h1 v-if="awesome">awesome!</h1>
+        <h1 v-else>no</h1>
+    </div>
+</template>
---
+++ src/views/Exercise/ExerciseTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DirectiveVIf from './DirectiveVIf.vue'
+import DirectiveVElse from './DirectiveVElse.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Directive v-if</h4>
+        <DirectiveVIf />
+        <h4>Directive v-else</h4>
+        <DirectiveVElse />
+    </div>
+</template>
