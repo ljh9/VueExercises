e230b38b46f74b4a22270a170e932be6e80b267b
ljh9 <dkssud2208@naver.com>
2022-06-10 20:30:51 +0900

    DirectiveVIf

---
+++ src/views/Exercise/DirectiveVIf.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+        awesome: true,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h1 v-if="awesome">awesome</h1>
+    </div>
+</template>
