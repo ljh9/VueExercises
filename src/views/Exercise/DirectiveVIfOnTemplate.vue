07a521289a5209cee921b37ecf7f7c2c2d434a24
ljh9 <dkssud2208@naver.com>
2022-06-11 22:04:11 +0900

    DirectiveVIfOnTemplate

---
+++ src/views/Exercise/DirectiveVIfOnTemplate.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        ok: true,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <template v-if="ok">
+            <h1>Title</h1>
+            <p>11111111111</p>
+            <p>222222222222</p>
+        </template>
+    </div>
+</template>
