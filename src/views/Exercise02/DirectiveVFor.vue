159a350465d765668c3c235b513625cf6a86a012
ljh9 <dkssud2208@naver.com>
2022-06-12 20:27:38 +0900

    DirectiveVFor

---
+++ src/views/Exercise02/DirectiveVFor.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+        items: [{ message: '11111111111' }, { message: '22222222222222' }]
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <li v-for="item in items" v-bind:key="item">
+            {{ item.message }}
+        </li>
+    </div>
+</template>
---
+++ src/views/Exercise02/ExerciseTest02.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DirectiveVFor from './DirectiveVFor.vue'
+</script>
+
+<template>
+    <div>
+        <h2>Exercise 06</h2>
+        <h4>Directive v-for</h4>
+        <DirectiveVFor />
+    </div>
+</template>
