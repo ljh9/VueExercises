5b5b6d3a3a80faa42157a3d4879406433cb17f81
ljh9 <dkssud2208@naver.com>
2022-06-12 20:27:06 +0900

    DirectiveVElseIf

---
+++ src/views/Exercise/DirectiveVElseIf.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+        type: 'Y',
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div v-if="type === 'A'">
+            A
+        </div>
+        <div v-else-if="type === 'B'">
+            B
+        </div>
+        <div v-else-if="type === 'C'">
+            C
+        </div>
+        <div v-else>
+            Not A/B/C
+        </div>
+    </div>
+</template>
--- src/views/Exercise/ExerciseTest.vue
+++ src/views/Exercise/ExerciseTest.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DirectiveVIf from './DirectiveVIf.vue'
 import DirectiveVElse from './DirectiveVElse.vue'
+import DirectiveVElseIf from './DirectiveVElseIf.vue'
 import DirectiveVIfOnTemplate from './DirectiveVIfOnTemplate.vue'
 import DirectiveVShow from './DirectiveVShow.vue'
 </script>
@@ -11,6 +12,8 @@ import DirectiveVShow from './DirectiveVShow.vue'
         <DirectiveVIf />
         <h4>Directive v-else</h4>
         <DirectiveVElse />
+        <h4>Directive v-else-if</h4>
+        <DirectiveVElseIf />
         <h4>Directive v-if on template</h4>
         <DirectiveVIfOnTemplate />
         <h4>Directive v-show</h4>
