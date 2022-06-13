ac53d7f4d65b8d17b367601476e62a31644a1bb4
ljh9 <dkssud2208@naver.com>
2022-06-13 22:05:43 +0900

    DirectiveVForParentScope

---
+++ src/views/Exercise02/DirectiveVForParentScope.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            parentMessage: 'Parent',
+            items: [{ message: 'aaaa' }, { message: 'bbbb' }]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <li v-for="(item, index) in items" v-bind:key="item"> 
+            {{ parentMessage }} - {{ index }} - {{ item.message }}
+        </li>
+    </div>
+</template>
