15a7b3eee1153f1765e9426cf9a46edc6904ab5e
ljh9 <dkssud2208@naver.com>
2022-06-14 20:18:20 +0900

    DirectiveVForOf

---
+++ src/views/Exercise02/DirectiveVForOf.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+        items: [{ message: 'Foo' }, { message: 'Bar' }]
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <li v-for="item of items" v-bind:key="item">
+            {{ item.message }}
+        </li>
+    </div>
+</template>
