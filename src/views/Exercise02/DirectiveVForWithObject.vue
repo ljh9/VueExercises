dc8909f54d8c26b31d73ed50836f7a6182e2c213
ljh9 <dkssud2208@naver.com>
2022-06-14 20:18:29 +0900

    DirectiveVForWithObject

---
+++ src/views/Exercise02/DirectiveVForWithObject.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            myObject: {
+            aa: 'Vue1',
+            bb: 'Vue2',
+            cc: 'Vue3'
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <ul>
+            <li v-for="value in myObject" v-bind:key="value">
+                {{ value }}
+            </li>
+        </ul>
+    </div>
+</template>
