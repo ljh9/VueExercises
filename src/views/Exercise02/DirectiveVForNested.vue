c5848b0317423012c63d388c371ce0147a238b88
ljh9 <dkssud2208@naver.com>
2022-06-14 20:18:11 +0900

    DirectiveVForNested

---
+++ src/views/Exercise02/DirectiveVForNested.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        items: [{ message: 'Foo', children: 'Vue' }, { message: 'Bar', children: 'Js' }]
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <li v-for="item in items" v-bind:key="item">
+            <span v-for="childItem in item.children" v-bind:key="childItem">
+                {{ item.message }} {{ childItem }}
+            </span>
+        </li>
+    </div>
+</template>
