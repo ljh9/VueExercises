ed167f95053301559c864280b586db1c482cae80
ljh9 <dkssud2208@naver.com>
2022-06-16 20:13:06 +0900

    DirectiveVForKey

---
+++ src/views/Exercise02/DirectiveVForKey.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            items: [{ id: 1, name: '111' }, { id: 2, name: '222' }, { id: 3, name: '333' }, { id: 4, name: '444' }]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div v-for="item in items" :key="item.id">
+            {{ item.name }}     
+        </div>
+    </div>
+</template>
