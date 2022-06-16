7993803a395eca4f0561ac8c3a01e9c89580fdf0
ljh9 <dkssud2208@naver.com>
2022-06-16 20:12:40 +0900

    DirectiveVForWithVIf

---
+++ src/views/Exercise02/DirectiveVForWithVIf.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            todos: [{ name: '111' }, { name: '222' }, { name: '333' }, { name: '444' }]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <template v-for="todo in todos" v-bind:key="todo">
+            <li v-if="!todo.isComplete">
+                {{ todo.name }} 
+            </li>
+        </template>
+    </div>
+</template>
