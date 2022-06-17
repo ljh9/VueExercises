362860d28af2baa5256207349a882f4f2d07909c
ljh9 <dkssud2208@naver.com>
2022-06-17 21:25:33 +0900

    DirectiveVForComponent

---
+++ src/views/Exercise02/DirectiveVForComponent.vue
@@ -0,0 +1,22 @@
+<script>
+import MyComponent from './MyComponent.vue'
+export default {
+    components: { MyComponent },
+    data() {
+        return {
+            items: [{ id: 1, name: '111' }, { id: 2, name: '222' }, { id: 3, name: '333' }, { id: 4, name: '444' }]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <my-component
+            v-for="(item, index) in items"
+            :item="item"
+            :index="index"
+            :key="item.id"
+        ></my-component>
+    </div>
+</template>
---
+++ src/views/Exercise02/MyComponent.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+	props: ['item']
+}
+</script>
+
+<template>
+  <li>
+    {{ item.name }}
+  </li>
+</template>
