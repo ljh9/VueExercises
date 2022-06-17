b9d70b6a3634bf4343f8554b467d5e3ed7471493
ljh9 <dkssud2208@naver.com>
2022-06-17 21:25:54 +0900

    DirectiveVForComputed

---
+++ src/views/Exercise02/DirectiveVForComputed.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            numbers: [1, 2, 3, 4, 5]
+        }
+    },
+    computed: {
+        evenNumbers() {
+            return this.numbers.filter(n => n % 2 === 0)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <li v-for="n in evenNumbers" v-bind:key="n">{{ n }}</li>
+    </div>
+</template>
