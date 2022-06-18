bba6c669613657dae91de736892aaa6309e77978
ljh9 <dkssud2208@naver.com>
2022-06-18 19:51:13 +0900

    DirectiveVOn

---
+++ src/views/Exercise03/DirectiveVOn.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="count++">Add 1</button>
+        <p>Count is: {{ count }}</p>
+    </div>
+</template>
