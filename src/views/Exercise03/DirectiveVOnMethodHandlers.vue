38c2f31874a23962b103ef3470b51a06c678b5e9
ljh9 <dkssud2208@naver.com>
2022-06-18 19:51:31 +0900

    DirectiveVOnMethodHandlers

---
+++ src/views/Exercise03/DirectiveVOnMethodHandlers.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'Vue.js'
+        }
+    },
+    methods: {
+        greet(event) {
+            // `this` inside methods points to the current active instance
+            alert(`Hello ${this.name}!`)
+            // `event` is the native DOM event
+            if (event) {
+                alert(event.target.tagName)
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <!-- `greet` is the name of the method defined above -->
+        <button @click="greet">Greet</button>
+    </div>
+</template>
