c1c2f27a9f95f9e67c46ee21e12bf5241676e527
ljh9 <dkssud2208@naver.com>
2022-06-18 19:51:43 +0900

    DirectiveVOnCallMethodInline

---
+++ src/views/Exercise03/DirectiveVOnCallMethodInline.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    methods: {
+        say(message) {
+            alert(message)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="say('hello')">Say hello</button>
+        <button @click="say('bye')">Say bye</button>
+    </div>
+</template>
---
+++ src/views/Exercise03/ExerciseTest03.vue
@@ -0,0 +1,16 @@
+<script setup>
+import DirectiveVOn from './DirectiveVOn.vue'
+import DirectiveVOnMethodHandlers from './DirectiveVOnMethodHandlers.vue'
+import DirectiveVOnCallMethodInline from './DirectiveVOnCallMethodInline.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Directive v-on</h4>
+        <DirectiveVOn />
+        <h4>Directive v-on with method handlers</h4>
+        <DirectiveVOnMethodHandlers />
+        <h4>Calling Methods in Inline Handlers</h4>
+        <DirectiveVOnCallMethodInline />
+    </div>
+</template>
