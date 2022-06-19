d6af77965500bda9ac6515be4d106502e2337882
ljh9 <dkssud2208@naver.com>
2022-06-19 21:32:34 +0900

    DirectiveVOnEventArgument

---
+++ src/views/Exercise03/DirectiveVOnEventArgument.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    methods: {
+        warn(message, event) {
+            // now we have access to the native event
+            if (event) {
+                event.preventDefault()
+            }
+            alert(message)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <!-- using $event special variable -->
+        <button @click="warn('Form cannot be submitted yet.', $event)">
+            Submit
+        </button>
+
+        <!-- using inline arrow function -->
+        <button @click="(event) => warn('Form cannot be submitted yet.', event)">
+            Submit
+        </button>
+    </div>
+</template>
