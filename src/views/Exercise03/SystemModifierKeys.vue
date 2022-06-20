268905982e086904ab2b82040f5b06d0266e029c
ljh9 <dkssud2208@naver.com>
2022-06-20 22:13:24 +0900

    SystemModifierKeys

---
+++ src/views/Exercise03/SystemModifierKeys.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    methods: {
+        clear() {
+            document.getElementById("inputClear").value = "";
+        },
+        doSomething() {
+            alert(event.target.tagName);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <!-- Alt + Enter -->
+        <input @keyup.alt.enter="clear" id="inputClear" />
+
+        <!-- Ctrl + Click -->
+        <div @click.ctrl="doSomething">Do something</div>
+    </div>
+</template>
