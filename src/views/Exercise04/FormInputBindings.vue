3b5527f39985ef29e754272063b2555c72e3ad4e
ljh9 <dkssud2208@naver.com>
2022-06-21 21:26:39 +0900

    FormInputBindings

---
+++ src/views/Exercise04/FormInputBindings.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            text: 'text'
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input
+            :value="text"
+            @input="event => text = event.target.value">
+        <br />
+        {{ text }}
+        <br />
+        <input v-model="text">
+    </div>
+</template>
