4c27b3a7b9191a743a80b9209a15f52c226b7390
ljh9 <dkssud2208@naver.com>
2022-06-22 02:56:44 +0900

    MultilineText

---
+++ src/views/Exercise04/MultilineText.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            message: ''
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <span>Multiline message is:</span>
+        <p style="white-space: pre-line;">{{ message }}</p>
+        <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    </div>
+</template>
