e4df8cfd333808f38a24cafb0c9f97dc97eba2cf
ljh9 <dkssud2208@naver.com>
2022-06-22 02:56:55 +0900

    CheckBox

---
+++ src/views/Exercise04/SingleCheckBox.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            checked: false
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input type="checkbox" id="checkbox" v-model="checked" />
+        <label for="checkbox">{{ checked }}</label>
+    </div>
+</template>
