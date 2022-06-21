7a6b8e5c164a6c2a88b015397853391a9074f3e9
ljh9 <dkssud2208@naver.com>
2022-06-21 21:27:03 +0900

    TextTest

---
+++ src/views/Exercise04/ExerciseTest04.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FormInputBindings from './FormInputBindings.vue'
+import TextTest from './TextTest.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Form Input Bindings</h4>
+        <FormInputBindings />
+        <h4>Text</h4>
+        <TextTest />
+    </div>
+</template>
---
+++ src/views/Exercise04/TextTest.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            message: ''
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <p>Message is: {{ message }}</p>
+        <input v-model="message" placeholder="edit me" />
+    </div>
+</template>
