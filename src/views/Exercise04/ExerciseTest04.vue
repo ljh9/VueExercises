eade09910328693f5fee149c16d7ce1a56f6d728
ljh9 <dkssud2208@naver.com>
2022-06-22 02:57:05 +0900

    MultipleCheckboxes

--- src/views/Exercise04/ExerciseTest04.vue
+++ src/views/Exercise04/ExerciseTest04.vue
@@ -1,6 +1,9 @@
 <script setup>
 import FormInputBindings from './FormInputBindings.vue'
 import TextTest from './TextTest.vue'
+import MultilineText from './MultilineText.vue'
+import SingleCheckBox from './SingleCheckBox.vue'
+import MultipleCheckboxes from './MultipleCheckboxes.vue'
 </script>
 
 <template>
@@ -9,5 +12,11 @@ import TextTest from './TextTest.vue'
         <FormInputBindings />
         <h4>Text</h4>
         <TextTest />
+        <h4>Multiline text</h4>
+        <MultilineText />
+        <h4>Checkbox</h4>
+        <SingleCheckBox />
+        <h4>Multiple Checkbox</h4>
+        <MultipleCheckboxes />
     </div>
 </template>
