4dc4c29a71b30241d6ba0990b00bddfd4b3d47d7
ljh9 <dkssud2208@naver.com>
2022-06-23 19:31:55 +0900

    RadioButton

---
+++ src/views/Exercise04/RadioButton.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      picked: ""
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div>Picked: {{ picked }}</div>
+
+        <input type="radio" id="one" value="One" v-model="picked" />
+        <label for="one">One</label>
+
+        <input type="radio" id="two" value="Two" v-model="picked" />
+        <label for="two">Two</label>
+    </div>
+</template>
