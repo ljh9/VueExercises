d2c39823ba686346b60e234df0910dffcc9487fe
ljh9 <dkssud2208@naver.com>
2022-06-24 21:31:52 +0900

    BindingSelectOptions

---
+++ src/views/Exercise04/BindingSelectOptions.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      selected: 'A',
+      options: [
+        { text: 'One', value: 'A' },
+        { text: 'Two', value: 'B' },
+        { text: 'Three', value: 'C' }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <select v-model="selected">
+            <option v-for="option in options" :value="option.value" v-bind:key="option">
+                {{ option.text }}
+            </option>
+        </select>
+
+        <div>Selected: {{ selected }}</div>
+    </div>
+</template>
