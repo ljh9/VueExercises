21ee4683160595d4b8a7a853a147260d4a1bc724
ljh9 <dkssud2208@naver.com>
2022-06-24 21:32:03 +0900

    CheckboxValueBindings

---
+++ src/views/Exercise04/CheckboxValueBindings.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      toggle: ""
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input
+            type="checkbox"
+            v-model="toggle"
+            true-value="yes"
+            false-value="no" />
+
+        <div>Value: {{ toggle }}</div>
+    </div>
+</template>
