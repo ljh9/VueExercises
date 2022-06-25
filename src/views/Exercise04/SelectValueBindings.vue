8c1a80eba9e6a525f3fa2c72ab0aef6c44be7cf4
ljh9 <dkssud2208@naver.com>
2022-06-25 22:38:46 +0900

    SelectValueBindings

---
+++ src/views/Exercise04/SelectValueBindings.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      selected: ""
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <select v-model="selected">
+            <!-- inline object literal -->
+            <option :value="{ number: 123 }">123</option>
+        </select>
+
+        <div>Value: {{ selected }}</div>
+    </div>
+</template>
