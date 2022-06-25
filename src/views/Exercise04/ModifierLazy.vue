0cfd6e9439ca93bb047f0cdf862e10b904f143ce
ljh9 <dkssud2208@naver.com>
2022-06-25 22:38:56 +0900

    ModifierLazy

---
+++ src/views/Exercise04/ModifierLazy.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: '',
+            text: ''
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        Default: <input v-model="text">
+        {{ text }}
+        <br/>
+        Lazy: <input v-model.lazy="msg" />
+        {{ msg }}
+    </div>
+</template>
