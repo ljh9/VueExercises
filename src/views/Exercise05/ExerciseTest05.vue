76c06bf3fa99a0fc612f9685623d204817a2c849
ljh9 <dkssud2208@naver.com>
2022-06-26 21:36:26 +0900

    LifecycleHooks

---
+++ src/views/Exercise05/ExerciseTest05.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LifecycleHooks from './LifecycleHooks.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Lifecycle Hooks</h4>
+        <LifecycleHooks />
+    </div>
+</template>
---
+++ src/views/Exercise05/LifecycleHooks.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            message: ''
+        }
+    },
+    created() {
+        console.log(`the component is now created.`)
+    },
+    mounted() {
+        console.log(`the component is now mounted.`)
+    },
+    updated() {
+        console.log(`the component is now updated.`)
+    }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <span>Multiline message is:</span>
+        <p style="white-space: pre-line;">{{ message }}</p>
+        <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    </div>
+</template>
