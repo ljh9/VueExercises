7771281c2551f503d116dc6a8ed84cf47db74d66
ljh9 <dkssud2208@naver.com>
2022-06-27 23:01:42 +0900

    BasicExample

---
+++ src/views/Exercise06/BasicExample.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      question: '',
+      answer: 'Questions usually contain a question mark. ;-)'
+    }
+  },
+  watch: {
+    // whenever question changes, this function will run
+    question(newQuestion, oldQuestion) {
+      if (newQuestion.indexOf('?') > -1) {
+        this.oldQuestion = oldQuestion
+        this.getAnswer()
+      }
+    }
+  },
+  methods: {
+    async getAnswer() {
+      this.answer = 'Thinking...'
+      try {
+        const res = await fetch('https://yesno.wtf/api')
+        this.answer = (await res.json()).answer
+      } catch (error) {
+        this.answer = 'Error! Could not reach the API. ' + error
+      }
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <p>
+            Ask a yes/no question:
+            <input v-model="question" />
+        </p>
+        <p>{{ answer }}</p>
+    </div>
+</template>
---
+++ src/views/Exercise06/ExerciseTest06.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BasicExample from './BasicExample.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Basic Example</h4>
+        <BasicExample />
+    </div>
+</template>
