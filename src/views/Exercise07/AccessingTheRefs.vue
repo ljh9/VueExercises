20548638e7553ac2baef8fa1c196aa23407afc96
ljh9 <dkssud2208@naver.com>
2022-07-09 01:35:57 +0900

    AccessingTheRefs

---
+++ src/views/Exercise07/AccessingTheRefs.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.input.focus()
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+      <input ref="input" />
+    </div>
+</template>
