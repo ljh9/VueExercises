8313f2c1e8e755556d435b421a5c28c018aac64b
ljh9 <dkssud2208@naver.com>
2022-07-09 01:36:08 +0900

    RefsInsideVFor

---
+++ src/views/Exercise07/ExerciseTest07.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AccessingTheRefs from './AccessingTheRefs.vue'
+import RefsInsideVFor from './RefsInsideVFor.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Accessing The Refs</h4>
+        <AccessingTheRefs />
+        <h4>Refs inside v-for</h4>
+        <RefsInsideVFor />
+    </div>
+</template>
---
+++ src/views/Exercise07/RefsInsideVFor.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        1, 2, 3, 4, 5, 6
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+      <ul>
+        <li v-for="item in list" ref="items" v-bind:key="item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+</template>
