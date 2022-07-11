1c03adf63326fd2fd31c990ce577fb3776d52c47
ljh9 <dkssud2208@naver.com>
2022-07-12 00:20:33 +0900

    DefiningComponent

---
+++ src/views/Exercise08/DefiningComponent.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="count++">You clicked me {{ count }} times.</button>
+    </div>
+</template>
---
+++ src/views/Exercise08/ExerciseTest08.vue
@@ -0,0 +1,10 @@
+<script setup>
+import DefiningComponent from './DefiningComponent.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Defining a Component</h4>
+        <DefiningComponent />
+    </div>
+</template>
