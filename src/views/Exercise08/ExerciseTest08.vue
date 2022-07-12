e13ec7185ef01c6844bac56da95d9d9de152463f
ljh9 <dkssud2208@naver.com>
2022-07-13 01:00:06 +0900

    UsingComponentNTimes

--- src/views/Exercise08/ExerciseTest08.vue
+++ src/views/Exercise08/ExerciseTest08.vue
@@ -1,10 +1,16 @@
 <script setup>
 import DefiningComponent from './DefiningComponent.vue'
+import UsingComponent from './UsingComponent.vue'
+import UsingComponentNTimes from './UsingComponentNTimes.vue'
 </script>
 
 <template>
     <div>
         <h4>Defining a Component</h4>
         <DefiningComponent />
+        <h4>Using a Component</h4>
+        <UsingComponent />
+        <h4>Using Components</h4>
+        <UsingComponentNTimes />
     </div>
 </template>
---
+++ src/views/Exercise08/UsingComponentNTimes.vue
@@ -0,0 +1,17 @@
+<script>
+import ButtonCounter from './ButtonCounter.vue'
+export default {
+  components: {
+    ButtonCounter
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h1>Here are many child components</h1>
+        <ButtonCounter />
+        <ButtonCounter />
+        <ButtonCounter />
+    </div>
+</template>
