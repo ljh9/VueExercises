ffa938fa5d6d05fa897f8766a432bacae9822382
ljh9 <dkssud2208@naver.com>
2022-07-13 00:59:52 +0900

    UsingComponent

---
+++ src/views/Exercise08/UsingComponent.vue
@@ -0,0 +1,15 @@
+<script>
+import ButtonCounter from './ButtonCounter.vue'
+export default {
+  components: {
+    ButtonCounter
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h1>Here is a child component</h1>
+        <ButtonCounter />
+    </div>
+</template>
