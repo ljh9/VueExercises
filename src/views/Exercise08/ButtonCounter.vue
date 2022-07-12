0f9cdabc96edce16f0c0ff5e1bdd0cb6ea174744
ljh9 <dkssud2208@naver.com>
2022-07-13 00:59:31 +0900

    ButtonCounter

---
+++ src/views/Exercise08/ButtonCounter.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="count++">
+            You clicked me {{ count }} times
+        </button>
+    </div>
+</template>
