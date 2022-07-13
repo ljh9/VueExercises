b5a25abe21cb1660b7184ec36226cedb11dd19ac
ljh9 <dkssud2208@naver.com>
2022-07-14 00:12:46 +0900

    UsingProps

---
+++ src/views/Exercise08/UsingProps.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BlogPost from './BlogPost.vue'   
+</script>
+
+<template>
+    <div class="exercise">
+        <BlogPost title="111111111111" />
+        <BlogPost title="222222222222" />
+        <BlogPost title="333333333333" />
+    </div>
+</template>
