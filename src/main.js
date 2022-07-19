d2bb814437e646390cb365a06bdc1015a24f525a
ljh9 <dkssud2208@naver.com>
2022-07-19 20:10:30 +0900

    GlobalRegistration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+
+// createApp(App).use(router).mount('#app')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ComponentA from './views/Exercise09/ComponentA.vue'
+import ComponentB from './views/Exercise09/ComponentB.vue'
+import ComponentC from './views/Exercise09/ComponentC.vue'
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+.component('ComponentA', ComponentA)
+.component('ComponentB', ComponentB)
+.component('ComponentC', ComponentC);
+app.use(router);
+app.mount('#app');
---
+++ src/views/Exercise09/ExerciseTest09.vue
@@ -0,0 +1,10 @@
+<script setup>
+import GlobalRegistration from './GlobalRegistration.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Global Registration</h4>
+        <GlobalRegistration/>
+    </div>
+</template>
