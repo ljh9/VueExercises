3defcd7a53284e84e0db9c4c2da8c6a8b6a7867c
ljh9 <dkssud2208@naver.com>
2022-07-21 00:08:14 +0900

    PropsDeclaration

---
+++ src/views/Exercise10/Exercise10.vue
@@ -0,0 +1,10 @@
+<script setup>
+import PropsDeclaration from './PropsDeclaration.vue'
+</script>
+
+<template>
+    <div>
+        <h4>Props Declaration</h4>
+        <PropsDeclaration foo="Props Declaration!@#!@#???????"/>
+    </div>
+</template>
---
+++ src/views/Exercise10/PropsDeclaration.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: ['foo'],
+  created() {
+    console.log(this.foo)
+    
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        Check the console
+    </div>
+</template>
