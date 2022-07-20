9e1ccb254fe60f0674e8d75b6ca4968ede375057
ljh9 <dkssud2208@naver.com>
2022-07-20 18:43:18 +0900

    LocalRegistration

---
+++ src/views/Exercise09/LocalRegistration.vue
@@ -0,0 +1,14 @@
+<script>
+import ComponentA from './ComponentA.vue'
+export default {
+  components: {
+    ComponentA
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <ComponentA />
+    </div>
+</template>
