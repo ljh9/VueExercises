30ff5c7bf2f8f4e35eaad83e17fc5c86a1127a8d
ljh9 <dkssud2208@naver.com>
2022-07-22 00:07:32 +0900

    PropsDeclarationObject

---
+++ src/views/Exercise10/PropsDeclarationObject.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    title: String,
+    likes: Number
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h6>{{title}}</h6>
+        {{likes}} likes.
+        
+    </div>
+</template>
