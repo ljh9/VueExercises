c9de3f96a8addf73dd80d737735c9cf9be6183ec
ljh9 <dkssud2208@naver.com>
2022-07-24 00:11:17 +0900

    BlogPostObject

---
+++ src/views/Exercise10/BlogPostObject.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  props: ['title', 'id']
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h4>{{ id }} - {{ title }}</h4>
+        
+    </div>
+</template>
