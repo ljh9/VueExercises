c8e79347d774fbaf8ac0f8854dbcf18f79eb7c6e
ljh9 <dkssud2208@naver.com>
2022-07-25 00:58:33 +0900

    ChildComponentForDataFlow

---
+++ src/views/Exercise10/ChildComponentForDataFlow.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        props: ['initialCounter2', 'size2'],
+        data() {
+            return {
+                // counter only uses this.initialCounter as the initial value;
+                // it is disconnected from future prop updates.
+                counter: this.initialCounter2
+            }
+        },
+        computed: {
+            // computed property that auto-updates when the prop changes
+            normalizedSize() {
+                return this.size2.trim().toLowerCase()
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="exercise">
+        initial value of Counter: {{ counter }}
+        <br/>
+        Size: {{ normalizedSize }}
+        
+    </div>
+</template>
